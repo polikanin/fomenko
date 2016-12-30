8ced44a06d4a26bbb20dec4b6c799a34af93edb2
Anatoliy <papik5@mail.ru>
2016-12-30 22:04:04 +0300

    add video bg

--- script/js.js
+++ script/js.js
@@ -1,180 +1,193 @@
-var requared = $('input[data-required]');
-var modal = '.modal';
-var form = $('form');
+$(document).ready(function () {
+    $('.preloader').addClass('preloaded');
 
-$('[name="phone"]').inputmask("+7(999)9999999");
-requared.blur(function() {var self = $(this);if($(this).val().length == "") {self.addClass('input_error');setTimeout(function () {self.removeClass('input_error')}, 2000)}});
-requared.focus(function() {$(this).removeClass('input_error');});
+    var requared = $('input[data-required]');
+    var modal = '.modal';
+    var form = $('form');
 
-var close = function () {
-    $(modal).addClass('hidden');
-    $(modal + '>div:not(.layout)').addClass('hidden');
-};
+    $('[name="phone"]').inputmask("+7(999)9999999");
+    requared.blur(function() {var self = $(this);if($(this).val().length == "") {self.addClass('input_error');setTimeout(function () {self.removeClass('input_error')}, 2000)}});
+    requared.focus(function() {$(this).removeClass('input_error');});
 
-var open = function () {
-    $(modal).removeClass('hidden');
-    $($(this).data('modal')).removeClass('hidden')
-};
+    var close = function () {
+        $(modal).addClass('hidden');
+        $(modal + '>div:not(.layout)').addClass('hidden');
+    };
 
-form.submit(function(e){
-    e.preventDefault();
-    var self = $(this);
-    var requared = true;
-    var inputs = self.find('[data-required]');
+    var open = function () {
+        $(modal).removeClass('hidden');
+        $($(this).data('modal')).removeClass('hidden')
+    };
 
-    $('[name="frm-name"]').val(self.attr('name'));
+    form.submit(function(e){
+        e.preventDefault();
+        var self = $(this);
+        var requared = true;
+        var inputs = self.find('[data-required]');
 
-    for(var i = 0; i < inputs.length; i++){
-        if(inputs.eq(i).val() == '') {
-            requared = false;
-        }
-    }
-    if(requared){
-        var type = self.attr('method');
-        var url = self.attr('action');
-        var data = self.serialize();
-        $.ajax({type: type, url: url, data: data,
-            success : function(){
-                $('form input:not([type="hidden"])').val('');
-                $(modal).removeClass('hidden');
-                $(modal + '>div:not(.layout)').addClass('hidden');
-                $('.modal .thanks').removeClass('hidden');
-                console.log('Success');
-            }
-        });
-    }
-    else{
+        $('[name="frm-name"]').val(self.attr('name'));
+
         for(var i = 0; i < inputs.length; i++){
             if(inputs.eq(i).val() == '') {
-                inputs.eq(i).addClass('input_error');
-                setTimeout(function () {
-                    inputs.removeClass('input_error');
-                }, 2000);
+                requared = false;
             }
         }
-    }
-});
+        if(requared){
+            var type = self.attr('method');
+            var url = self.attr('action');
+            var data = self.serialize();
+            $.ajax({type: type, url: url, data: data,
+                success : function(){
+                    $('form input:not([type="hidden"])').val('');
+                    $(modal).removeClass('hidden');
+                    $(modal + '>div:not(.layout)').addClass('hidden');
+                    $('.modal .thanks').removeClass('hidden');
+                    console.log('Success');
+                }
+            });
+        }
+        else{
+            for(var i = 0; i < inputs.length; i++){
+                if(inputs.eq(i).val() == '') {
+                    inputs.eq(i).addClass('input_error');
+                    setTimeout(function () {
+                        inputs.removeClass('input_error');
+                    }, 2000);
+                }
+            }
+        }
+    });
 
 
 //// скрипт закрывающий форму
-$('[data-btn-type="close"]').on('click', close);
+    $('[data-btn-type="close"]').on('click', close);
 
 //// скрипт открывающий форму
-$('[data-modal]').on('click', open);
+    $('[data-modal]').on('click', open);
 
-var nextBtn = '<button class="slider-btn slider-btn_next"><span class="arr-passive"></span><span class="arr-active"></span></button>';
-var prevBtn = '<button class="slider-btn slider-btn_prev"><span class="arr-passive"></span><span class="arr-active"></span></button>';
+    var nextBtn = '<button class="slider-btn slider-btn_next"><span class="arr-passive"></span><span class="arr-active"></span></button>';
+    var prevBtn = '<button class="slider-btn slider-btn_prev"><span class="arr-passive"></span><span class="arr-active"></span></button>';
 
 //// Слайдер
-if($('.slider')){
-    $('.slider').slick({
-        slidesToShow: 1,
-        dots: true,
-        prevArrow: prevBtn,
-        nextArrow: nextBtn
-    });
-}
+    if($('.slider')){
+        $('.slider').slick({
+            slidesToShow: 1,
+            dots: true,
+            prevArrow: prevBtn,
+            nextArrow: nextBtn
+        });
+    }
 
 //// Галерея на слайдере
-$('.gallery-item').fancybox();
+    $('.gallery-item').fancybox();
 
-$('[data-toggle]').on('click', function(){
-    var data = $(this).data('toggle');
-    $(this).toggleClass(data);
-});
-
-$('.play-video').on('click', function (e) {
-    e.preventDefault();
-    var videoId = $(this).attr('href');
-    var videoTpl = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/'+ videoId +'?autoplay=1" frameborder="0"  allowfullscreen></iframe>';
-    var parent = $(this).parent();
-    parent.addClass('active');
-    parent.find('.video-block').html(videoTpl);
-});
-
-$('.video').each(function () {
-    var href = $(this).find('.play').attr('href');
-    var img = $(this).css({
-        backgroundImage: 'url(//img.youtube.com/vi/'+ href +'/hqdefault.jpg)'
+    $('[data-toggle]').on('click', function(){
+        var data = $(this).data('toggle');
+        $(this).toggleClass(data);
     });
-});
 
-$(window).on('scroll', function () {
-    if($(window).scrollTop() > 300){
-        $('.header_mobile').addClass('show');
-    }
-    else{
-        $('.header_mobile').removeClass('show');
-    }
-});
+    $('.play').on('click', function (e) {
+        e.preventDefault();
+        var videoId = $(this).attr('href');
+        var videoTpl = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/'+ videoId +'?autoplay=1" frameborder="0"  allowfullscreen></iframe>';
+        var parent = $(this).parent();
+        parent.addClass('active');
+        parent.find('.video-block').html(videoTpl);
+    });
 
-if($('.gallery-preview-block')){
-    $('.gallery-preview-block .item').each(function () {
-        var self = $(this);
-        var container = self.find('.pic-container');
-        var url = self.find('.pic').attr('src');
-        container.css({
-            backgroundImage: "url("+ url +")"
+    $('.video').each(function () {
+        var href = $(this).find('.play').attr('href');
+        var img = $(this).css({
+            backgroundImage: 'url(//img.youtube.com/vi/'+ href +'/hqdefault.jpg)'
         });
-        self.find('.pic').remove();
     });
-}
+
+    $(window).on('scroll', function () {
+        if($(window).scrollTop() > 300){
+            $('.header_mobile').addClass('show');
+        }
+        else{
+            $('.header_mobile').removeClass('show');
+        }
+    });
+
+    if($('.gallery-preview-block')){
+        $('.gallery-preview-block .item').each(function () {
+            var self = $(this);
+            var container = self.find('.pic-container');
+            var url = self.find('.pic').attr('src');
+            container.css({
+                backgroundImage: "url("+ url +")"
+            });
+            self.find('.pic').remove();
+        });
+    }
 
 
-$('[data-track]').on('click', function () {
-    var url = $(this).data('track');
+    $('[data-track]').on('click', function () {
+        var url = $(this).data('track');
 
-    $('#player source').attr('src', url);
-    console.log(url)
-});
-var audiojs;
+        $('#player source').attr('src', url);
+        console.log(url)
+    });
 
-if(audiojs){
-    $(function() {
-        // Setup the player to autoplay the next track
-        var a = audiojs.createAll({
-            trackEnded: function() {
-                var next = $('ol li.playing').next();
-                if (!next.length) next = $('ol li').first();
-                next.addClass('playing').siblings().removeClass('playing');
-                audio.load($('a', next).attr('data-src'));
+    if(audiojs){
+        $(function() {
+            // Setup the player to autoplay the next track
+            var a = audiojs.createAll({
+                trackEnded: function() {
+                    var next = $('ol li.playing').next();
+                    if (!next.length) next = $('ol li').first();
+                    next.addClass('playing').siblings().removeClass('playing');
+                    audio.load($('a', next).attr('data-src'));
+                    audio.play();
+                }
+            });
+
+            // Load in the first track
+            var audio = a[0];
+            first = $('ol a').attr('data-src');
+            $('ol li').first().addClass('playing');
+            audio.load(first);
+
+            // Load in a track on click
+            $('ol li').click(function(e) {
+                e.preventDefault();
+                $(this).addClass('playing').siblings().removeClass('playing');
+                audio.load($('a', this).attr('data-src'));
                 audio.play();
-            }
-        });
+            });
 
-        // Load in the first track
-        var audio = a[0];
-        first = $('ol a').attr('data-src');
-        $('ol li').first().addClass('playing');
-        audio.load(first);
+            var getTrackName = function () {
+                var trackName = $('ol .playing a').text();
+                $('.current-track').text(trackName);
+            };
 
-        // Load in a track on click
-        $('ol li').click(function(e) {
-            e.preventDefault();
-            $(this).addClass('playing').siblings().removeClass('playing');
-            audio.load($('a', this).attr('data-src'));
-            audio.play();
+            $('.ctrl-btn-prev').on('click', function () {
+                getTrackName();
+                var prev = $('li.playing').prev();
+                if (!prev.length) prev = $('ol li').last();
+                prev.click();
+            });
+            $('.ctrl-btn-next').on('click', function () {
+                getTrackName();
+                var next = $('li.playing').next();
+                if (!next.length) next = $('ol li').first();
+                next.click();
+            });
+            $('.audiojs .play-pause').on('click', getTrackName);
+            $('ol li').on('click', getTrackName);
+            $('.play-pause').html('');
         });
+    }
 
-        var getTrackName = function () {
-            var trackName = $('ol .playing a').text();
-            $('.current-track').text(trackName);
-        };
+    var isMobile = false;
 
-        $('.ctrl-btn-prev').on('click', function () {
-            getTrackName();
-            var prev = $('li.playing').prev();
-            if (!prev.length) prev = $('ol li').last();
-            prev.click();
-        });
-        $('.ctrl-btn-next').on('click', function () {
-            getTrackName();
-            var next = $('li.playing').next();
-            if (!next.length) next = $('ol li').first();
-            next.click();
-        });
-        $('.audiojs .play-pause').on('click', getTrackName);
-        $('ol li').on('click', getTrackName);
-        $('.play-pause').html('');
-    });
-}
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        isMobile = true;
+    }
+
+    if(isMobile){
+        $('.video-bg-wrap').remove();
+    }
+});
